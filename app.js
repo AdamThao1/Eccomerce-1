0664c2831ce4bb1660adbf1c2e1df78becd9dd79
AdamThao1 <athao215@gmail.com>
2023-05-10 15:59:46 -0400

    fixed some javascript

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const preBtn = document.querySelectorAll('.pre-btn');
 //line 2 selects all the elements in the product container and stores them in the variable
 //line 3-4 selects the elements in the nxt/pre button and stores them in their own vairable
 productContainers.forEach(function(item, i) { //loops through each product container with this function
-    let containerDimensions = item.getBoundingClientRect(); //this would get the dimensions
-    let containerWidth = containerDimensions.width; //this would get the width
+  let containerDimensions = item.getBoundingClientRect(); //this would get the dimensions
+  let containerWidth = containerDimensions.width; //this would get the width
 
-    nxtBtn[i].addEventListener('click', function() { //event listeners for the click on the button
-        item.scrollLeft += containerWidth; //when button is clicked it would scroll button to the right
-    });
-
-    preBtn[i].addEventListener('click', function() { //event listners for the click on the button
-        item.scrollLeft -= containerWidth; //when button is click it would scroll button to the left
-    });
+  nxtBtn[i].addEventListener('click', function() { //event listeners for the click on the button
+    item.scrollLeft += containerWidth; //when button is clicked it would scroll button to the right
+  });  
+  preBtn[i].addEventListener('click', function() { //event listners for the click on the button
+    item.scrollLeft -= containerWidth; //when button is click it would scroll button to the left
+  });
 });
 
 //responsives to home page
@@ -24,19 +23,19 @@ const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if(bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
 }
 if(close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
 }
 
 //adding JSON
 
-let http = new XMLHttpRequest();
+const http = new XMLHttpRequest();
 
 http.open('get', 'image.json', true);
 
@@ -44,11 +43,11 @@ http.send();
 
 http.onload = function(){
     if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText);
-        let output = "";
-        for(let item of products){
-            output += `
-                <div class="product">
+  const products = JSON.parse(this.responseText);
+    let output = "";
+    for(let item of products){
+        output += `
+      <div class="product">
                     <img src="${item.image}" alt="${item.image}">
                     <p class="title">${item.title}</p>
                     <p class="description">${item.description}</p>
@@ -59,6 +58,6 @@ http.onload = function(){
                 </div>
             `;
         }
-        document.querySelector(".products").innerHTML = output;
+    document.querySelector(".products").innerHTML = output;
     }
-}
+  }
